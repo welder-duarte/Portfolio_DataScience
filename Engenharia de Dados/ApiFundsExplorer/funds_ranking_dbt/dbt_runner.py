e76a079bb32986ce7756f722ed26ffe4f37e69f6
welder-duarte <welder228@hotmail.com>
2026-02-26 16:25:44 -0300

    ajuste dbt_runner.py

--- Engenharia de Dados/ApiFundsExplorer/funds_ranking_dbt/dbt_runner.py
+++ Engenharia de Dados/ApiFundsExplorer/funds_ranking_dbt/dbt_runner.py
@@ -1,21 +1,23 @@
-import subprocess
-from flask import Flask, jsonify
+import subprocess, json, logging
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 @app.route("/", methods=["POST"])
 def run_dbt():
-    result = subprocess.run(
-        ["dbt", "run"],
-        capture_output=True,
-        text=True,
-        cwd="/app")
+    logging.info("DBT execution started")
 
-    return jsonify({
-        "returncode": result.returncode,
-        "stdout": result.stdout,
-        "stderr": result.stderr
-    }), 200
+    result = subprocess.run(["dbt", "run"], capture_output=True, text=True, cwd="/app")
+
+    log_payload = {"returncode": result.returncode,"stdout": result.stdout,"stderr": result.stderr}
+
+    if result.returncode != 0:
+        logging.error(json.dumps(log_payload))
+        return jsonify({"status": "error"}), 500
+
+    logging.info(json.dumps(log_payload))
+    return jsonify({"status": "success"}), 200
 
 
 if __name__ == "__main__":
